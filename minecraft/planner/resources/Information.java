24eec0ede7ac0111a38c914084675038d9becbb3
gelo2056-ai <gelo2056@gmail.com>
2026-06-08 09:34:46 +0300

    Add files via upload

---
+++ minecraft/planner/resources/Information.java
@@ -0,0 +1,23 @@
+package minecraft.planner.resources;
+
+public class Information {
+   public static final String RESOURCE_PACKAGE = "/minecraft/planner/resources/";
+   public static final String VERSION = "0.99.7";
+   public static final String COPYRIGHT = "(c) Neil Vickers, 2010 - 2011";
+   public static final String FILE_SUFFIX = "xsp";
+   public static final String FILE_SCHEMATIC_SUFFIX = "schematic";
+   public static final String FILE_BINVOX_SUFFIX = "binvox";
+   public static final String FILE_RUINS_SUFFIX = "tml";
+   public static final String FILE_IMAGE_SUFFIX = "png";
+   public static final String FILE_TERRAIN = "terrain.png";
+   public static final String PROPERTY_DISABLE3D = "disable3D";
+   public static final String PROPERTY_DISABLELOADER = "disableLoader";
+   public static final String VERSION_URL = "http://minecraftstructureplanner.com/download/version.txt";
+   public static final String DOWNLOAD_URL = "http://minecraftstructureplanner.com/index.php/download";
+   public static final String CONFIGURATION_DIRECTORY = ".MinecraftStructurePlanner";
+   public static final String TEXTURE_SET_CONFIG = "texture_sets.cfg";
+   public static final String TEXTURE_PACK_CONFIG = "texture_packs.cfg";
+   public static final String TEXTURE_PACK_DIRECTORY = "TexturePacks";
+   public static final String TEXTURE_PACK_SUFFIX = "tpk";
+   public static final int TEXTURE_SIZE = 32;
+}
---
+++ minecraft/planner/resources/XML.java
@@ -0,0 +1,16 @@
+package minecraft.planner.resources;
+
+public class XML {
+   public static final String GRID_TAG = "grid";
+   public static final String CELL_TAG = "cell";
+   public static final String BLOCK_TAG = "block";
+   public static final String STRUCTURE_TAG = "structure";
+   public static final String NOTES_TAG = "notes";
+   public static final String X_ATTRIBUTE = "x";
+   public static final String Y_ATTRIBUTE = "y";
+   public static final String WIDTH_ATTRIBUTE = "width";
+   public static final String HEIGHT_ATTRIBUTE = "height";
+   public static final String MAX_HEIGHT_ATTRIBUTE = "maxheight";
+   public static final String TEXTURE_ATTRIBUTE = "texture";
+   public static final String ORIENTATION_ATTRIBUTE = "orientation";
+}
